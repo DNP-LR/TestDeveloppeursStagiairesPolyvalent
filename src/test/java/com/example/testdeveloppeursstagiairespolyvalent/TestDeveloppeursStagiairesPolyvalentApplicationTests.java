e99bbe6ba795bdcf7787ac2bcb82c96f62a4407d
Donfack Pavel <noubissiepavel@gmail.com>
2023-11-10 22:04:07 +0100

    init with database config

---
+++ src/test/java/com/example/testdeveloppeursstagiairespolyvalent/TestDeveloppeursStagiairesPolyvalentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdeveloppeursstagiairespolyvalent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeveloppeursStagiairesPolyvalentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
